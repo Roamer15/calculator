4a7862d6a4018d1a393861970cb139631665607f
Beleke Ian <ianseeksbeta@gmail.com>
2024-12-22 16:55:50 +0100

    JS linter fixed

--- js/index.js
+++ js/index.js
@@ -1,25 +1,29 @@
 const display = document.getElementById('display-screen')
 
-let keyPressed = document.querySelectorAll('numbers-et-al')
+const keyPressed = document.querySelectorAll('numbers-et-al')
 
-function displayOnScreen(input) {
-    display.value += input
+function displayOnScreen (input) {
+  display.value += input
 }
 
-function clearScreen() {
-    display.value = ''
+function clearScreen () {
+  display.value = ''
 }
 
-function calculate() {
+function calculate () {
+  try {
     display.value = eval(display.value)
+  } catch (error) {
+    display.value = 'Error'
+  }
 }
 
-function binaryConversion (){
-    let number = display.value
-    let conversion = []
-    while (number > 0) {
-        conversion.unshift(number % 2)
-        number = Math.floor(number / 2)
-    }
-    display.value = conversion.join('')
+function binaryConversion () {
+  let number = display.value
+  const conversion = []
+  while (number > 0) {
+    conversion.unshift(number % 2)
+    number = Math.floor(number / 2)
+  }
+  display.value = conversion.join('')
 }
